b75f95b55446abb13d342abfa7e93466bc2a6f1a
Dikshitha <104306196+Deekshu966@users.noreply.github.com>
2025-02-25 11:18:31 +0530

    Time: 4 ms (48.94%), Space: 12.6 MB (39.06%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -11,25 +11,15 @@ class Solution(object):
         :type postorder: List[int]
         :rtype: Optional[TreeNode]
         """
-        post_idx = {val: i for i, val in enumerate(postorder)}  # Create a hashmap for postorder indices
-
-        def build(pre_start, pre_end, post_start, post_end):
-            if pre_start > pre_end:
+        def build(preorder,postorder,root):
+            if not preorder and not postorder:
                 return None
-            
-            root = TreeNode(preorder[pre_start])  # Create root node
-            
-            if pre_start == pre_end:
+            root = TreeNode(preorder[0])
+            if len(preorder) == 1:
                 return root
-
-            left_child_val = preorder[pre_start + 1]
-            left_child_idx = post_idx[left_child_val]  # Find index of left child in postorder
-            left_size = left_child_idx - post_start + 1  # Size of the left subtree
-
-            # Recursively build left and right subtrees
-            root.left = build(pre_start + 1, pre_start + left_size, post_start, left_child_idx)
-            root.right = build(pre_start + left_size + 1, pre_end, left_child_idx + 1, post_end - 1)
-
-            return root
-
-        return build(0, len(preorder) - 1, 0, len(postorder) - 1)
+            left_val = preorder[1]
+            mid = postorder.index(left_val)+1  
+            root.left = build(preorder[1:mid+1], postorder[:mid],root.left)
+            root.right = build(preorder[mid+1:],postorder[mid:-1],root.right)
+            return root 
+        return build(preorder,postorder,None)
