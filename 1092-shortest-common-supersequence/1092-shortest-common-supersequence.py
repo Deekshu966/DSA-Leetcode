5580062046f0dfca4386a7554d12d352e0be380e
Dikshitha <104306196+Deekshu966@users.noreply.github.com>
2025-03-01 00:08:04 +0530

    Time: 374 ms (94.4%), Space: 42.8 MB (74.21%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,43 +1,56 @@
-class Solution(object):
-    def shortestCommonSupersequence(self, str1, str2):
-        """
-        :type str1: str
-        :type str2: str
-        :rtype: str
-        """
-        # Step 1: Compute the LCS using DP
-        m, n = len(str1), len(str2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+    
+        # First LCS problem cheyyali
 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if str1[i - 1] == str2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+        n = len(str1)
+        m = len(str2)
+
+        
+        dp = [[0] * (m+1) for i in range(n+1)]
+
+        
+        for i in range(n):
+            for j in range(m):
+
+                if(str1[i] == str2[j]):
+                    dp[i+1][j+1] = 1 + dp[i][j]
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                    dp[i+1][j+1] = max(dp[i+1][j],dp[i][j+1])
+        
 
-        # Step 2: Construct the Shortest Common Supersequence (SCS)
-        i, j = m, n
-        result = []
+        answer = ""
 
-        while i > 0 and j > 0:
-            if str1[i - 1] == str2[j - 1]:  # If characters match, take it
-                result.append(str1[i - 1])
-                i -= 1
-                j -= 1
-            elif dp[i - 1][j] > dp[i][j - 1]:  # Move towards the larger LCS value
-                result.append(str1[i - 1])
-                i -= 1
-            else:
-                result.append(str2[j - 1])
-                j -= 1
+        # ippudu bottom up approach cheyyali
+        i = n
+        j = m
 
-        # Add remaining characters from str1 or str2
-        while i > 0:
-            result.append(str1[i - 1])
-            i -= 1
-        while j > 0:
-            result.append(str2[j - 1])
+        while(i > 0 and j > 0):
+
+                if(str1[i-1] == str2[j-1]):
+                    answer += str1[i-1]
+                    j -= 1
+                    i -= 1
+
+                else:
+                    if(dp[i-1][j] > dp[i][j-1]):
+                        answer += str1[i-1]
+                        i -= 1
+                    else:
+                        answer += str2[j-1]
+                        j -= 1
+        
+        while(j > 0):
+            answer += str2[j-1]
             j -= 1
+        
+        while(i > 0):
+            answer += str1[i-1]
+            i -= 1
+        
+        return answer[::-1]
 
-        return "".join(result[::-1])  # Reverse because we constructed backwards
+
+
+                    
+
